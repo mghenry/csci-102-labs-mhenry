8ef8420834c57ee952e42824c2752151e0d2a77b
mghenry <mghenry1499@gmail.com>
2020-11-30 10:19:31 -0700

    load_file initial function, still needs to be tested

--- cs102labs/Week12-utility.py
+++ cs102labs/Week12-utility.py
@@ -3,3 +3,10 @@
 #   Week 12 - Utility using Git and Incremental Development
 #   References: none
 #   Time: 1 hour
+
+
+def load_file(str(some_file_name)):
+    file_list = some_file_name.readlines()
+    some_file_name.close()
+    print('OUTPUT', file_list)
+
